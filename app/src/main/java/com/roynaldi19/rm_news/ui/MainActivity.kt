4dfe94e52bf200e7c7d5b5ee35bc267bd9c9673c
roynaldi19 <roynaldi19@gmail.com>
2024-01-27 12:29:43 +0700

    add loading to 2 berita

--- app/src/main/java/com/roynaldi19/rm_news/ui/MainActivity.kt
+++ app/src/main/java/com/roynaldi19/rm_news/ui/MainActivity.kt
@@ -1,21 +1,16 @@
 package com.roynaldi19.rm_news.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearSnapHelper
-import com.roynaldi19.rm_news.R
 import com.roynaldi19.rm_news.data.retrofit.ApiConfig
-import com.roynaldi19.rm_news.data.retrofit.ApiService
 import com.roynaldi19.rm_news.databinding.ActivityMainBinding
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -30,18 +25,21 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         topHeadlineAdapter = TopHeadlineAdapter()
-        val topHeadLineLayoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        val topHeadLineLayoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
 
         everythingAdapter = EverythingAdapter()
         val everythingLayoutManager = LinearLayoutManager(this)
 
         binding.rvTopHeadline.layoutManager = topHeadLineLayoutManager
-        val topHeadLineItemDecoration = DividerItemDecoration(this, topHeadLineLayoutManager.orientation)
+        val topHeadLineItemDecoration =
+            DividerItemDecoration(this, topHeadLineLayoutManager.orientation)
         binding.rvTopHeadline.addItemDecoration(topHeadLineItemDecoration)
         binding.rvTopHeadline.adapter = topHeadlineAdapter
 
         binding.rvEverything.layoutManager = everythingLayoutManager
-        val everyThingItemDecoration = DividerItemDecoration(this, everythingLayoutManager.orientation)
+        val everyThingItemDecoration =
+            DividerItemDecoration(this, everythingLayoutManager.orientation)
         binding.rvEverything.addItemDecoration(everyThingItemDecoration)
         binding.rvEverything.adapter = everythingAdapter
 
@@ -56,7 +54,9 @@ class MainActivity : AppCompatActivity() {
         showEverything(query, apiKey)
     }
 
-    private fun showTopHeadline(country : String, apiKey: String) {
+    private fun showTopHeadline(country: String, apiKey: String) {
+        binding.pbTopHeadline.visibility = View.VISIBLE
+
         lifecycleScope.launch {
             try {
                 val apiService = ApiConfig.getApiService()
@@ -68,19 +68,20 @@ class MainActivity : AppCompatActivity() {
                     if (articles != null) {
                         topHeadlineAdapter.submitList(articles)
                     }
+                    binding.pbTopHeadline.visibility = View.GONE
                 } else {
                     Log.e("MainActivity", "Error: ${response.message()}")
                 }
-            } catch (e: Exception){
+            } catch (e: Exception) {
                 Log.e("MainActivity", "Exception: ${e.message}")
 
-            } finally {
-                showLoading(false)
             }
         }
     }
 
-    private fun showEverything(query: String, apiKey: String){
+    private fun showEverything(query: String, apiKey: String) {
+        binding.pbEverything.visibility = View.VISIBLE
+
         lifecycleScope.launch {
             try {
                 val apiService = ApiConfig.getApiService()
@@ -89,26 +90,18 @@ class MainActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     val articles = response.body()?.articles
 
-                    if (articles != null){
+                    if (articles != null) {
                         everythingAdapter.submitList(articles)
                     }
+                    binding.pbEverything.visibility = View.GONE
                 } else {
                     Log.e("MainActivity", "Error: ${response.message()}")
                 }
-            } catch (e: Exception){
+            } catch (e: Exception) {
                 Log.e("MainActivity", "Exception: ${e.message}")
 
-            } finally {
-                showLoading(false)
             }
         }
     }
 
-    private fun showLoading(isLoading: Boolean) {
-        if (isLoading) {
-            binding.progressBar.visibility = View.VISIBLE
-        } else {
-            binding.progressBar.visibility = View.GONE
-        }
-    }
 }
